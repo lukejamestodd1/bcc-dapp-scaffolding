30930104e4c801abfa61cd56ae3045a7a943246b
cryptoseabook <cryptoseabook@gmail.com>
2018-04-14 14:55:28 +1000

    add show room function

--- src/js/app.js
+++ src/js/app.js
@@ -38,6 +38,7 @@ App = {
       }).then(owner => {
         App.c_owner = owner;
         App.getBalances(owner);
+        App.reloadRooms();
       });
     })
   },
@@ -69,7 +70,7 @@ App = {
   },
 
   offerRoom: function() {
-      console.log("offer room");
+
       // retrieve details of the room
       var _room_name = $("#room-name").val();
       var _room_price = parseFloat($("#room-price").val() )|| 0;
@@ -84,24 +85,101 @@ App = {
       }
 
       return App.contracts.BCCRoomBooking.deployed().then(function(instance) {
-        instance.offerRoom(_room_name,
+        console.log("offer room");
+        return instance.offerRoom(_room_name,
           _room_description, _room_size, _room_price);
       }).then(function(result){
         console.log(result);
       }).catch(function(err){
         console.error(err);
       })
-
-
   },
 
   reloadRooms: function() {
+    // avoid reentry
+    if (App.loading) {
+      return;
+    }
+    App.loading = true;
+
+    // refresh account information because the balance may have changed
+
+    var appInstance;
+
+    App.contracts.BCCRoomBooking.deployed().then(function(instance) {
+      appInstance = instance;
+      return appInstance.getRoomsForBooking();
+    }).then(function(roomIds) {
+      // Retrieve and clear the room placeholder
+      console.log(roomIds, roomIds.length);
+      var roomRow = $('#roomsRow');
+      roomRow.empty();
+
+      for (var i = 0; i < roomIds.length; i++) {
+        var roomId = roomIds[i];
+        console.log(roomId);
+        appInstance.rooms(roomId).then(function(room) {
+          App.displayRoom(
+            room[0], // id
+            room[1], // owner
+            room[2], // booking person
+            room[3], // name
+            room[4], // desc
+            room[5], // size
+            room[6] // price
+          );
+        });
+      }
+      App.loading = false;
+    }).catch(function(err) {
+      console.log(err.message);
+      App.loading = false;
+    });
   },
 
   listenToEvents: function() {
   },
 
   displayRoom: function(id, owner, bookingPerson, name, description, size, price) {
+    var roomsRow = $('#roomsRow');
+    console.log(id, owner, bookingPerson, name, description);
+
+    // Retrieve and fill the room template
+    var roomTemplate = $('#roomTemplate');
+    roomTemplate.find('.panel-title').text(name);
+    roomTemplate.find('.room-price').text(price);
+    roomTemplate.find('.room-size').text(size);
+    roomTemplate.find('.room-description').text(description);
+    roomTemplate.find('.btn-booking').attr('data-id', id);
+    roomTemplate.find('.btn-booking').attr('data-value', price);
+
+    if (owner == App.c_owner) {
+      var bookingPersonAddress = bookingPerson.substring(0, 5) + "..." + bookingPerson.slice(-5);
+      roomTemplate.find('.room-booked-by').text(bookingPersonAddress);
+      roomTemplate.find('.btn-booking').prop("disabled",true);
+      if (bookingPerson.slice(-5).toString() != "00000") {
+        roomTemplate.find('.panel-body').addClass("booked");
+        roomTemplate.find('.btn-booking').text("Booked");
+      } else {
+        roomTemplate.find('.panel-body').removeClass("booked");
+        roomTemplate.find('.btn-booking').text("Available");
+      }
+
+    } else {
+      if (bookingPerson == "0x0000000000000000000000000000000000000000") {
+        roomTemplate.find('.room-booked-by').text("n/a");
+        roomTemplate.find('.btn-booking').text("Available");
+        roomTemplate.find('.btn-booking').addClass("btn-success");
+        roomTemplate.find('.btn-booking').prop("disabled",false);
+      } else {
+        roomTemplate.find('.room-booked-by').text(bookingPerson.substring(0, 5) + "..." + bookingPerson.slice(-5));
+        roomTemplate.find('.btn-booking').text("Booked");
+        roomTemplate.find('.btn-booking').prop("disabled",true);
+
+      }
+    }
+    
+    roomsRow.append(roomTemplate.html());
   },
 
   bookRoom: function() {
